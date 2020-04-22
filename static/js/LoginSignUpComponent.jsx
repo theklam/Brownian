2c9f6f529604e46624cfe26dfc0200cb979fce00
Kalvin Lam <kalvin_lam@brown.edu>
2020-04-22 18:24:01 -0400

    login works with hitting the post endpoints

--- static/js/LoginSignUpComponent.jsx
+++ static/js/LoginSignUpComponent.jsx
@@ -8,12 +8,62 @@ import {
 } from "react-router-dom";
 
 export default class LoginSignUpComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+
+        this.handleChangeUsername = this.handleChangeUsername.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleSignup = this.handleSignup.bind(this);
+    }
+
+    handleChangeUsername(event) {
+        this.setState({ username: event.target.value });
+    }
+
+    handleChangePassword(event) {
+        this.setState({ password: event.target.value });
+    }
+
+    handleLogin(e) {
+        console.log('The signup link was clicked.');
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: this.state.username, password: this.state.password })
+        };
+        fetch('/login', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
+    handleSignup(e) {
+        console.log('The signup link was clicked.');
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: this.state.username, password: this.state.password })
+        };
+        fetch('/signup', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+    }
+
+
     render() {
         return (
             <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Email" value={this.state.username} onChange={this.handleChangeUsername} />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -21,37 +71,18 @@ export default class LoginSignUpComponent extends React.Component {
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.handleChangePassword} />
                 </Form.Group>
                 <Form.Group controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="primary" onClick={handleLogin}>
+                <Button variant="primary" onClick={this.handleLogin}>
                     Login
+                </Button>
+                <Button variant="primary" onClick={this.handleSignup}>
+                    Signup
                 </Button>
             </Form>
         );
     }
-}
-
-// the function should handle the login by querying the database
-// to check if the login is correct, if the login is correct, then
-// we redirect user to main page. 
-// if login is incorrect we ask the user to enter another login shit. 
-function handleLogin(e) {
-    console.log('The link was clicked.');
-    fetch("/login")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                console.log(result);
-                console.log('api came back hella clean');
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-                console.log('error with api call');
-            }
-        )
 }
