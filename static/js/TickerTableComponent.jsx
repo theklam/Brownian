e33109a4ca5cff14733b353e8f46c8dcc31ab3c4
Kalvin Lam <kalvin_lam@brown.edu>
2020-04-23 19:58:10 -0400

    the stonks table is updating according to the backend

--- static/js/TickerTableComponent.jsx
+++ static/js/TickerTableComponent.jsx
@@ -8,41 +8,7 @@ import {
 } from "react-router-dom";
 import TickerRow from "./TickerRowComponent";
 
-export default class TickerTableComponent extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoaded: false,
-            items: []
-        };
-    }
-
-    componentDidMount() {
-        fetch("/holdings")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                    console.log(typeof(result));
-                    console.log(typeof(result[0]));
-                    console.log(Object.values(result));
-                    this.setState({
-                        isLoaded: true,
-                        items: Object.values(result)
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
+export default class TickerTableComponent extends React.Component { 
 
     render() {
         return (
@@ -56,7 +22,7 @@ export default class TickerTableComponent extends React.Component {
                     </tr>
                 </thead>
                 <tbody>
-                    {this.state.items.map((item) =>
+                    {this.props.items.map((item) =>
                         <TickerRow key={item.ticker}
                             items={item} />
                     )}
---
+++ static/js/UpdateTickerTableFormComponent.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Form, Col, Button } from 'react-bootstrap';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+export default class UpdateTickerTableForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ticker: '',
+            quantity: ''
+        };
+        this.postNewStock = this.postNewStock.bind(this);
+        this.handleChangeTicker = this.handleChangeTicker.bind(this);
+        this.handleChangeQuantity = this.handleChangeQuantity.bind(this);
+    }
+
+    handleChangeTicker(event) {
+        this.setState({ ticker: event.target.value });
+    }
+
+    handleChangeQuantity(event) {
+        this.setState({ quantity: event.target.value });
+    }
+
+    postNewStock() {
+        // do the posting
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ ticker: this.state.ticker, quantity: this.state.quantity })
+        };
+        fetch('/holdings', requestOptions)
+            .then(response => this.props.fetchCurrentHoldings());
+            // .then(data => console.log(data));
+
+        // then refresh
+        // this.props.fetchCurrentHoldings();
+    }
+
+    render() {
+        return (
+            <Form>
+                <Form.Row>
+                    <Col>
+                        <Form.Control placeholder="Ticker" onChange={this.handleChangeTicker} />
+                    </Col>
+                    <Col>
+                        <Form.Control placeholder="Quantity" onChange={this.handleChangeQuantity} />
+                    </Col>
+                    <Button variant="primary" onClick={this.postNewStock}>
+                        Add Stonk
+                    </Button>
+                </Form.Row>
+            </Form>
+        );
+    }
+}
