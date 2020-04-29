ade96d9224fc85d9e4f9f3ddd9dd8c8c9280290d
Kalvin Lam <kalvin_lam@brown.edu>
2020-04-29 17:40:55 -0400

    automatically update price and moved the price data up to the app level

--- static/js/UpdatePricesButtonComponent.jsx
+++ static/js/UpdatePricesButtonComponent.jsx
@@ -1,32 +1,22 @@
 import React from "react";
 import { Form, Col, Button } from 'react-bootstrap';
 
-export default class UpdatePrices extends React.Component {
+export default class UpdatePricesButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            ticker: '',
-            quantity: ''
-        };
-        this.fetchPrices = this.fetchPrices.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    fetchPrices() {
-        let portfolio_stocks = this.props.items.map(a => a.ticker);
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ stocks: portfolio_stocks, freq: 'intraday'})
-        };
-        fetch('/prices', requestOptions)
-            .then(response => this.props.fetchCurrentHoldings());
+
+    handleClick() {
+        this.props.fetchPrices(this.props.items.map(a=>a.ticker));
     }
 
     render() {
         return (
             <Form>
                 <Form.Row>
-                    <Button variant="primary" onClick={this.fetchPrices}>
+                    <Button variant="primary" onClick={this.handleClick}>
                         Update Prices
                     </Button>
                 </Form.Row>
