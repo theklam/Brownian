2b74324675859c1a02bac1219a7820fe50467f71
Kalvin Lam <kalvin_lam@brown.edu>
2020-05-07 15:38:13 -0400

    trying the donut chart, might be better because cleaner

--- static/js/manage/visualizeStocks.js
+++ static/js/manage/visualizeStocks.js
@@ -1,6 +1,11 @@
 import * as d3 from 'd3';
 
 const draw = (data, div_title) => {
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ don't touch ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+    var divToSelect = "." + div_title;
+    d3.select(divToSelect + " > *").remove();
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ don't touch ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
     // set the dimensions and margins of the graph
     var width = 450
     var height = 450
@@ -10,13 +15,6 @@ const draw = (data, div_title) => {
     var radius = Math.min(width, height) / 2 - margin
 
     // append the svg object to the div called 'my_dataviz'
-    console.log(div_title);
-    var divToSelect = "." + div_title;
-    console.log(divToSelect);
-
-    // remove the old viz
-    d3.select(divToSelect + " > *").remove();
-
     var svg = d3.select(divToSelect)
         .append("svg")
         .attr("width", width)
@@ -24,27 +22,77 @@ const draw = (data, div_title) => {
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+    // Create dummy data
+    var data = { a: 9, b: 20, c: 30, d: 8, e: 12, f: 3, g: 7, h: 14 }
+
     // set the color scale
-    var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+    var color = d3.scaleOrdinal()
+        .domain(["a", "b", "c", "d", "e", "f", "g", "h"])
+        .range(d3.schemeDark2);
 
     // Compute the position of each group on the pie:
     var pie = d3.pie()
-        .value(function (d) { return d.total_value; })
-    var data_ready = pie(data);
+        .sort(null) // Do not sort group by size
+        .value(function (d) { return d.value; })
+    var data_ready = pie(d3.entries(data))
+
+    // The arc generator
+    var arc = d3.arc()
+        .innerRadius(radius * 0.5)         // This is the size of the donut hole
+        .outerRadius(radius * 0.8)
+
+    // Another arc that won't be drawn. Just for labels positioning
+    var outerArc = d3.arc()
+        .innerRadius(radius * 0.9)
+        .outerRadius(radius * 0.9)
 
     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
     svg
-        .selectAll('whatever')
+        .selectAll('allSlices')
         .data(data_ready)
         .enter()
         .append('path')
-        .attr('d', d3.arc()
-            .innerRadius(0)
-            .outerRadius(radius)
-        )
-        .attr('fill', function (d, i) { return (color(i)) })
-        .attr("stroke", "black")
+        .attr('d', arc)
+        .attr('fill', function (d) { return (color(d.data.key)) })
+        .attr("stroke", "white")
         .style("stroke-width", "2px")
         .style("opacity", 0.7)
+
+    // Add the polylines between chart and labels:
+    svg
+        .selectAll('allPolylines')
+        .data(data_ready)
+        .enter()
+        .append('polyline')
+        .attr("stroke", "black")
+        .style("fill", "none")
+        .attr("stroke-width", 1)
+        .attr('points', function (d) {
+            var posA = arc.centroid(d) // line insertion in the slice
+            var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+            var posC = outerArc.centroid(d); // Label position = almost the same as posB
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+            posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+            return [posA, posB, posC]
+        })
+
+    // Add the polylines between chart and labels:
+    svg
+        .selectAll('allLabels')
+        .data(data_ready)
+        .enter()
+        .append('text')
+        .text(function (d) { console.log(d.data.key); return d.data.key })
+        .attr('transform', function (d) {
+            var pos = outerArc.centroid(d);
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+            return 'translate(' + pos + ')';
+        })
+        .style('text-anchor', function (d) {
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            return (midangle < Math.PI ? 'start' : 'end')
+        })
+
 }
 export default draw
