85d677447ff952b9d67a6c1e3176403a5233904b
Kalvin Lam <kalvin_lam@brown.edu>
2020-05-08 00:16:24 -0400

    polyline and text added with animation

--- static/js/manage/visualizeStocks.js
+++ static/js/manage/visualizeStocks.js
@@ -2,6 +2,8 @@ import * as d3 from 'd3';
 
 var arc;
 var svg;
+var outerArc;
+var radius;
 
 function arcTween(a) {
     var i = d3.interpolate(this._current, a);
@@ -37,6 +39,57 @@ const update = (data) => {
         .style("opacity", 0.7)
 
     existingSlices.exit().remove();
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~ Part 2: polyline ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // this represents the existing slices
+    var existingSlices2 = svg.selectAll('polyline')
+        .data(data_ready, function (d) { return d.data.ticker; });
+
+    existingSlices2.transition().duration(500); // Smooth transition with arcTween
+
+    var newSlices2 = existingSlices2.enter()
+        .append('polyline');
+
+    var allSlices2 = newSlices2.merge(existingSlices2);
+    allSlices2.attr("stroke", "black")
+        .style("fill", "none")
+        .attr("stroke-width", 1)
+        .attr('points', function (d) {
+            var posA = arc.centroid(d) // line insertion in the slice
+            var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+            var posC = outerArc.centroid(d); // Label position = almost the same as posB
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+            posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+            return [posA, posB, posC]
+        })
+
+    existingSlices2.exit().remove();
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~ Part 3: text ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // this represents the existing slices
+    var existingSlices3 = svg.selectAll('text')
+        .data(data_ready, function (d) { return d.data.ticker; });
+
+    existingSlices3.transition().duration(500); // Smooth transition with arcTween
+
+    var newSlices3 = existingSlices3.enter()
+        .append('text');
+
+    var allSlices3 = newSlices3.merge(existingSlices3);
+    allSlices3.text(function (d) { console.log(d.data.ticker); return d.data.ticker })
+        .attr('transform', function (d) {
+            var pos = outerArc.centroid(d);
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+            return 'translate(' + pos + ')';
+        })
+        .style('text-anchor', function (d) {
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            return (midangle < Math.PI ? 'start' : 'end')
+        })
+    existingSlices3.exit().remove();
 }
 
 const draw = (data, div_title) => {
@@ -51,7 +104,7 @@ const draw = (data, div_title) => {
     var margin = 40
 
     // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-    var radius = Math.min(width, height) / 2 - margin
+    radius = Math.min(width, height) / 2 - margin
 
     svg = d3.select(divToSelect)
         .append("svg")
@@ -83,9 +136,9 @@ const draw = (data, div_title) => {
         .outerRadius(radius * 0.8)
 
     // Another arc that won't be drawn. Just for labels positioning
-    // var outerArc = d3.arc()
-    //     .innerRadius(radius * 0.9)
-    //     .outerRadius(radius * 0.9)
+    outerArc = d3.arc()
+        .innerRadius(radius * 0.9)
+        .outerRadius(radius * 0.9)
 
     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
     svg
@@ -102,40 +155,44 @@ const draw = (data, div_title) => {
         .style("opacity", 0.7)
 
     // Add the polylines between chart and labels:
-    // svg
-    //     .selectAll('allPolylines')
-    //     .data(data_ready)
-    //     .enter()
-    //     .append('polyline')
-    //     .attr("stroke", "black")
-    //     .style("fill", "none")
-    //     .attr("stroke-width", 1)
-    //     .attr('points', function (d) {
-    //         var posA = arc.centroid(d) // line insertion in the slice
-    //         var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
-    //         var posC = outerArc.centroid(d); // Label position = almost the same as posB
-    //         var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
-    //         posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-    //         return [posA, posB, posC]
-    //     })
+    svg
+        .selectAll('polyline')
+        .data(data_ready, function (d, i) {
+            return d.data.ticker;
+        })
+        .enter()
+        .append('polyline')
+        .attr("stroke", "black")
+        .style("fill", "none")
+        .attr("stroke-width", 1)
+        .attr('points', function (d) {
+            var posA = arc.centroid(d) // line insertion in the slice
+            var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+            var posC = outerArc.centroid(d); // Label position = almost the same as posB
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+            posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+            return [posA, posB, posC]
+        })
 
-    // // Add the polylines between chart and labels:
-    // svg
-    //     .selectAll('allLabels')
-    //     .data(data_ready)
-    //     .enter()
-    //     .append('text')
-    //     .text(function (d) { console.log(d.data.ticker); return d.data.ticker })
-    //     .attr('transform', function (d) {
-    //         var pos = outerArc.centroid(d);
-    //         var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-    //         pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
-    //         return 'translate(' + pos + ')';
-    //     })
-    //     .style('text-anchor', function (d) {
-    //         var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-    //         return (midangle < Math.PI ? 'start' : 'end')
-    //     })
+    // Add the polylines between chart and labels:
+    svg
+        .selectAll('text')
+        .data(data_ready, function (d, i) {
+            return d.data.ticker;
+        })
+        .enter()
+        .append('text')
+        .text(function (d) { console.log(d.data.ticker); return d.data.ticker })
+        .attr('transform', function (d) {
+            var pos = outerArc.centroid(d);
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+            return 'translate(' + pos + ')';
+        })
+        .style('text-anchor', function (d) {
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            return (midangle < Math.PI ? 'start' : 'end')
+        })
 
 }
 export { draw, update };
